d165d46a34d13cd698e412f848fabd24132a8e33
Ryan Ashton <djryanash@me.com>
2023-06-08 17:29:40 +0800

    Initial Commit

---
+++ DualSenseDemo/ViewController.swift
@@ -0,0 +1,21 @@
+// 
+//  ViewController.swift
+//  DualSenseDemo
+//
+//  Created by Ryan Ashton on 8/6/2023
+//  Copyright © 2023 Ryan Ashton. All rights reserved.
+//
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
